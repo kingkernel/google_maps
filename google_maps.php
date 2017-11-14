f1602bdd7e19579535d9719e804fef39ab1e54c0
Daniel José dos Santos <daniel.santos.ap@gmail.com>
2017-11-13 23:28:14 -0200

    finalmente a versão 0.02 da classe
    eu reduzi a um método e coloquei uma descrição do quê se espera de alguns atributos.

--- google_maps.php
+++ google_maps.php
@@ -3,29 +3,29 @@ class googlemaps{
  /*
 	Data criação: 		11/11/2017
 	última Alteração: 	12/11/2017
-	Autor: 			Daniel J. Santos
+	Autor: 				Daniel J. Santos
 	Nome Classe: 		googlemaps
-	versão: 		0.02
+	versão: 			0.02
 
 -- ################################################################################################
 Atributos: 
 		key 			[private]		A key de usuário do google maps obrigatorio agora.
-		functionName 		[public]		O nome da função que iniciará o mapa .
+		functionName 	[public]		O nome da função que iniciará o mapa .
 		idmap			[public] 		O id da div que conterá o mapa.
 		zoom 			[public]		O zoom do mapa.. padrão é 15
-		position 		[public] 		A posição inicial do centro do mapa. array com 2 
-								argumentos. "lat" e "lng" que são latitude e longitude
+		position 		[public] 		A posição inicial do centro do mapa. array com 2 ->
+										<- argumentos. "lat" e "lng" que são latitude e longitude
 		points 			[public] 		Array contendo os objetos que são os pontos no mapa.
 
 Métodos:
 		MountJs()		[public]		Método encarregado de retornar o javascipt para ser 
-								renderizado o mapa na tela
+										renderizado o mapa na tela
 -- ################################################################################################
  */
- 	private $key;					// string
+ 	private $key;						// string
  	public $functionName;				// string
- 	public $idmap;					// string
- 	public $zoom;					// int
+ 	public $idmap;						// string
+ 	public $zoom;						// int
  	public $position = array();			// array
  	public $points = array();			// array
 
@@ -56,22 +56,30 @@ Método: mountjs
 	versão: 			0.02
 
 -- ################################################################################################
+Atributos:
+		mapa 			[public]		Qual mapa pertence o objeto
+		icon 			[public]		A image que será mostrada no mapa. Recomendamos no tamanho ->
+										<- máximo de 48X48, embora qualquer tamanhom seja aceito
+		variablename 	[public]		O nome do objeto para diferenciar nos eventos
+		title 			[public]		Texto (alt) que aparece se passar o mouse sobre o ponto
+		position 		[public] 		Posição do objeto no mapa. array com latitude[lat] e ->
+										<- longitude [lng].
+		balloncontent	[public] 		Conteudo em HTML do balão, caso o usuário clique no ponto ->
+										<- do mapa
+		exibeballon		[public] 		caso seja true, exibe o balão se o ponto for clicado.
+-- ################################################################################################
  */
- 	public $mapa;				// a qual mapa esse ponto pertence
- 	public $icon;				// icone que será usado... recomento imagens 48X48
- 	public $name;				// ainda sem utilização... mas acho que será preciso
- 	public $title;				// texto para quando passar o mouse
- 	public $somacontent;		// ainda sem utilização... mas acho que será preciso
- 	public $position;			//array com latitude e longitude [lat], [lng]
- 	public $balloncontent;		// conteudo do balão de descrição
- 	public $variablename;		// nome da variavel que vai conter o balão
- 	public $exibeballon;
+ 	public $mapa;						// string
+ 	public $icon;						// string (caminho da imagem)
+ 	public $variablename;				// string
+ 	public $title;						// string
+ 	public $position;					// array
+ 	public $balloncontent;				// conteudo do balão de descrição
+ 	public $exibeballon;				// boolean [default: false]
 
 
  	public function __construct(){
- 		if(!isset($this->icon)){$this->icon='\'\'';};if(!isset($this->title)){$this->title='\'\'';};
- 		if(!isset($this->variablename)){$this->variablename='descri';};
- 		if(!isset($this->balloncontent)){$this->balloncontent='<div><h1>hellou!</h1></div>';};
+ 		if(!isset($this->icon)){$this->icon='\'\'';};if(!isset($this->title)){$this->title='\'\'';};if(!isset($this->variablename)){$this->variablename='descri';};if(!isset($this->balloncontent)){$this->balloncontent='<div><h1>hellou!</h1></div>';};
 
  	}
  	public function render(){
@@ -83,37 +91,4 @@ Método: mountjs
  	}
 
  }
-
- // exemplo de uso...
- //  <div id="map" ></div> 			---> o div que conterá o mapa esse id deve ser passado pra classe com  idmap
- 
- //declarando o mapa
-$maps = new googlemaps;
-$maps->functionname = "initMap";
-$maps->idmap = "map";
-$maps->position = array('lat' => "-0.035446", 'lng'=>"-51.1822884");
-
-	//declarando os pontos
-	$ponto1 = new mapspoint;
-	$ponto1->mapa = "map"; 			//------> qual mapa pertence esse ponto...
-	$ponto1->title = "ponto 1";
-	$ponto1->position = array("lat"=>"-0.035446", "lng"=>"-51.1822884");
-
-	$ponto2 = new mapspoint;
-	$ponto2->mapa = "map"; 			//------> qual mapa pertence esse ponto...
-	$ponto2->title = "ponto 2";
-	$ponto2->position = array("lat"=>"-0.02673", "lng"=>"-51.17817");
-
-	$ponto3 = new mapspoint;
-	$ponto3->mapa = "map"; 			//------> qual mapa pertence esse ponto...
-	$ponto3->title = "ponto 3";
-	$ponto3->position = array("lat"=>"-0.03355", "lng"=>"-51.17487");
-	
-	//adicionando os pontos no mapa
-	$maps->points = array($ponto1, $ponto2, $ponto3);
-
-// chamando o javascript	
-$maps->mountjs();
-// fazendo o mapa aparecer na tela
-$maps->render()
 ?>
